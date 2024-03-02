b0dcaf23995f90a3a6b9ca229c089e2985b188c3
Josh Murr <joshuadavidmurr@gmail.com>
2024-03-02 22:51:51 +0000

    Lazy + LSP stuff

--- lua/murr/init.lua
+++ lua/murr/init.lua
@@ -1,4 +1,4 @@
-local mods = {
+--[[ local mods = {
   'murr.compiled',
   'murr.core',
 }
@@ -12,4 +12,36 @@ for _, mod in ipairs(mods) do
   elseif not ok and not mod:find('murr.core.user') then
     error(('Error loading %s...\n\n%s'):format(mod, err))
   end
+
+end ]]
+
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+if not vim.loop.fs_stat(lazypath) then
+  vim.fn.system({
+    "git",
+    "clone",
+    "--filter=blob:none",
+    "https://github.com/folke/lazy.nvim.git",
+    "--branch=stable", -- latest stable release
+    lazypath,
+  })
 end
+vim.opt.rtp:prepend(lazypath)
+
+require("lazy").setup({
+  {
+    "folke/tokyonight.nvim",
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
+    priority = 1000, -- make sure to load this before all the other start plugins
+    config = function()
+      -- load the colorscheme here
+      vim.cmd([[colorscheme tokyonight]])
+    end,
+  },
+  { "nvim-tree/nvim-web-devicons", lazy = true },
+  { "nvim-lua/plenary.nvim" },
+  { "hrsh7th/cmp-nvim-lsp", },
+  { import = "murr.lsp.lspconfig" },
+  { import = "murr.lsp.none-ls" },
+  { import = "murr.lsp.mason" }
+})
