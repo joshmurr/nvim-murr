f8aff77332f838b74c224d42957d33caab3dc6d0
Josh Murr <joshuadavidmurr@gmail.com>
2024-09-24 14:46:58 +0100

    Add gitsigns

---
+++ lua/murr/plugins/gitsigns.lua
@@ -0,0 +1,17 @@
+local map = require('murr.utils').map
+
+return {
+  'lewis6991/gitsigns.nvim',
+  lazy = true,
+  config = function()
+    require('gitsigns').setup({
+      on_attach = function()
+        local gitsigns = require('gitsigns')
+
+        map('n', '<leader>hb', function()
+          gitsigns.blame_line({ full = false })
+        end)
+      end,
+    })
+  end,
+}
